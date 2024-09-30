e11a41829baee031d864fd9a8398d10742004818
Eileen199807 <158263464+Eileen199807@users.noreply.github.com>
2024-09-30 23:08:20 +0800

    Create mOTU.cmd
    First submitted

---
+++ mOTU.cmd
@@ -0,0 +1,64 @@
+#!/bin/bash
+#####################################################################################
+###                                                                                 #
+### slurm-mpi_omp.cmd :                                                             #
+### A SLURM submission script for running Hybrid MPI+OpenMP jobs on HPC2021 system  #
+###                                                                                 #
+### Compilation for Hybrid MPI+OpenMP program                                       #
+### (1) Intel MPI Libaries                                                          #
+###     module load impi                                                            #
+###     mpiifort -qopenmp mpi_omp_hello.f90 -o mpi_omp_hello-f90-impi               #
+### (2) OpenMPI Libaries                                                            #
+###     module load openmpi                                                         #
+###     mpif90 -fopenmp mpi_omp_hello.f90 -o mpi_omp_hello-f90-ompi                 #
+###                                                                                 #
+### Job submission                                                                  #
+###    cd to directory containing program/executable, then:                         #
+###    sbatch <location of this script>/slurm-mpi_omp.cmd                           #
+###                                                                                 #
+### SLURM for Multicore/Multi-thread: https://slurm.schedmd.com/mc_support.html     #
+###                                                                                 #
+### - Written by Lilian Chan, HKU ITS (2021-3-2)                                    #
+###                                                                                 #
+#####################################################################################
+#SBATCH --job-name=motus
+#SBATCH --mail-type=FAIL,END                      # Mail events
+#SBATCH --mail-user=lwleen@hku.hk                 # Update your email address
+#SBATCH --partition=intel                         # Specific Partition (intel/amd)
+#SBATCH --time=2-10:00:00                         # Wall time limit (days-hrs:min:sec)
+#SBATCH --ntasks=1                                # Total number of MPI tasks(processes)
+#SBATCH --nodes=1                                 # Total number of compute node(s)
+#SBATCH --ntasks-per-node=1                       # Number of MPI Tasks on each node
+#SBATCH --cpus-per-task=6                         # Number of CPUs per each MPI task
+#SBATCH --mem-per-cpu=12G                         # Memory setting
+#SBATCH --output=%x.out.%j                        # Standard output file
+#SBATCH --error=%x.err.%j                         # Standard error file
+#SBATCH --account=sph_pengwu                      # Which group to use sbs_ssin or sph_pengwu
+#####################################################################################
+### The following stuff will be executed in the first allocated node.               #
+### Please don't modify it                                                          #
+#####################################################################################
+echo "SLURMD_NODENAME       : $SLURMD_NODENAME"
+echo "SLURM_NTASKS          : $SLURM_NTASKS"
+echo "SLURM_JOB_NUM_NODES   : $SLURM_JOB_NUM_NODES"
+echo "SLURM_CPUS_PER_TASK   : $SLURM_CPUS_PER_TASK"
+echo "SLURM_CPUS_ON_NODE    : $SLURM_CPUS_ON_NODE"
+echo JOBID ${SLURM_JOBID} : ${SLURM_NTASKS} CPUs allocated from ${SLURM_JOB_NODELIST}
+echo Working directory is ${SLURM_SUBMIT_DIR}  1>&2
+echo This SLURM script is running on host ${SLURMD_NODENAME} 1>&2
+#################################################################################
+### Install environment using conda
+### conda install motus
+
+fastq_dir="/lustre1/g/sph_pengwu/tutorial/2023_RIF_WGS_workshop/workshop3/Eileen/07_WGS_Meta/02_Fastp/Clean_fastq/paired_reads/"
+unpair_dir="/lustre1/g/sph_pengwu/tutorial/2023_RIF_WGS_workshop/workshop3/Eileen/07_WGS_Meta/02_Fastp/Clean_fastq/unpaired_reads"
+output_dir="/lustre1/g/sph_pengwu/tutorial/2023_RIF_WGS_workshop/workshop3/Eileen/07_WGS_Meta/15_mOTUs"
+
+for SampleID in `cat sample.lst`
+do
+cd ${fastq_dir}/${SampleID}_remove_kraken
+motus profile -f ${SampleID}_fastp_1.fq.gz -r ${SampleID}_fastp_2.fq.gz -s ${unpair_dir}/${SampleID}_fastp_unpaired_1.fastq.gz -k genus -t 12 > ${output_dir}/${SampleID}_taxonomy_profile_genus.txt
+done
+
+#Merge profiles
+#motus merge -i CA034_taxonomy_profile_genus.txt,CB012_taxonomy_profile_genus.txt,CB014_taxonomy_profile_genus.txt,CB031_taxonomy_profile_genus.txt,CB038_taxonomy_profile_genus.txt,CB059_taxonomy_profile_genus.txt,CB158_taxonomy_profile_genus.txt,CB159_taxonomy_profile_genus.txt,CB161_taxonomy_profile_genus.txt,CB211_taxonomy_profile_genus.txt,CB646_131S_taxonomy_profile_genus.txt,CB646_212S_taxonomy_profile_genus.txt,CB711_taxonomy_profile_genus.txt,CB783_taxonomy_profile_genus.txt,CB850_taxonomy_profile_genus.txt,CB853_taxonomy_profile_genus.txt,CB895_taxonomy_profile_genus.txt,CB917_taxonomy_profile_genus.txt,CB918_taxonomy_profile_genus.txt,CB985_taxonomy_profile_genus.txt,CH010_taxonomy_profile_genus.txt,CH011_taxonomy_profile_genus.txt,CH015_taxonomy_profile_genus.txt,CX146_taxonomy_profile_genus.txt,CX149_taxonomy_profile_genus.txt,CZ033_taxonomy_profile_genus.txt,CZ050_taxonomy_profile_genus.txt,CZ120_taxonomy_profile_genus.txt,CZ121_taxonomy_profile_genus.txt -o HUM.motus
